e6ddfa5cb046ae45716fd7776211692961dc1cc3
KashyapGodambe <kashgodambe@gmail.com>
2024-05-09 18:50:53 +0530

    Adding Lab43,44 codes

--- src/ex_04052024/Lab043.java
+++ src/ex_04052024/Lab043.java
@@ -6,12 +6,22 @@ public class Lab043 {
 		// TODO String practise prg 1
 		//Reverse a String without using any function or using for loop.
 		
-		String original = "Hello World!";
+		String original = "Hello World!"; //12-1
         String reversed = "";
 
         for (int i = original.length() - 1; i >= 0; i--) {
             reversed += original.charAt(i);
         }
+        
+        //System.out.println(original.length());
+        
+        //System.out.println(original.charAt(11));
+//        
+//        The below for loop won't work, since indexing start from 0.
+        
+//        for (int i = original.length(); i > 0; i--) {
+//            reversed += original.charAt(i);
+//        }
 
         System.out.println("Original String: " + original);
         System.out.println("Reversed String: " + reversed);
--- src/ex_04052024/Lab044.java
+++ src/ex_04052024/Lab044.java
@@ -3,7 +3,9 @@ package ex_04052024;
 public class Lab044 {
 
 	public static void main(String[] args) {
-		// TODO find Duplicate characters in the String - Hello World -> Helowrd
+		// TODO find & remove Duplicate characters in the String - Hello World -> Helowrd
+		
+		
 
 	}
 
